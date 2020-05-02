81f7e7fba4187caad483537a55399a9c8b1507bb
Matt <mjruddy94@gmail.com>
2020-05-02 18:32:24 -0400

    change default to prod url

--- src/data/env.ts
+++ src/data/env.ts
@@ -1,11 +1,12 @@
 export const vars = () => ({
-  "env": {
+  env: {
     //local
     //"API_URL": "http://localhost:8080",
     //dev
-    "API_URL": "http://45.79.173.223:8080",
+    // "API_URL": "http://45.79.173.223:8080",
     //prod
-    // "API_URL": "https://progressiveapp.store/api",
-    "APP_SERVER_KEY": "BDH7_PrAmOw8oUj_NfGpUVVmwr70jUx5XY_l21P80qhBxaTGHbmxZA38O31R99oi6bcUpPDOZOBL7JKNYFkF2wo",
-  }
-});
+    API_URL: "/api",
+    APP_SERVER_KEY:
+      "BDH7_PrAmOw8oUj_NfGpUVVmwr70jUx5XY_l21P80qhBxaTGHbmxZA38O31R99oi6bcUpPDOZOBL7JKNYFkF2wo",
+  },
+})
