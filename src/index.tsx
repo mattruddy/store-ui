073e63f8faa13bdee1096db231b1cef0961a1ec9
Adam <champtoday@gmail.com>
2020-06-02 02:02:20 +0000

    add sw push

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,10 @@ import * as serviceWorker from "./serviceWorker"
 import { defineCustomElements } from "@ionic/pwa-elements/loader"
 import { setupConfig } from "@ionic/core"
 import App from "./App"
+import { ReduxCombinedState } from "./redux/RootReducer"
 const { store } = storeFactory()
+
+export type Store = typeof store
 
 // Show loading Screen for 1 second
 // const App = lazy(() =>
@@ -18,18 +21,18 @@ const { store } = storeFactory()
 // )
 
 setupConfig({
-  swipeBackEnabled: false,
+    swipeBackEnabled: false,
 })
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
 )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
+serviceWorker.register(store)
 defineCustomElements(window)
--- src/redux/User/actions.ts
+++ src/redux/User/actions.ts
@@ -1,418 +1,429 @@
 import {
-  UserState,
-  USER_SET_LOADING,
-  USER_SET_DATA,
-  USER_SET_PWAS,
-  USER_REPLACE_APP,
-  USER_ADD_APP,
-  USER_REMOVE_APP,
-  UserRole,
+    UserState,
+    USER_SET_LOADING,
+    USER_SET_DATA,
+    USER_SET_PWAS,
+    USER_REPLACE_APP,
+    USER_ADD_APP,
+    USER_REMOVE_APP,
+    UserRole,
 } from "./types"
-import { PWA } from "../../util/types"
+import { PWA, Push } from "../../util/types"
 import { ReduxCombinedState } from "../RootReducer"
 import { Action } from "redux"
 import { ThunkAction } from "redux-thunk"
 import {
-  getUserData,
-  setHasReadInstallStorage,
-  setEmailStorage,
-  setTokenStorage,
-  setUsernameStorage,
-  setIsLoggedInStorage,
-  Axios,
-  AxiosForm,
-  setRoleStorage,
-  UploadScreenshots,
-  DeleteScreenshot,
+    getUserData,
+    setHasReadInstallStorage,
+    setEmailStorage,
+    setTokenStorage,
+    setUsernameStorage,
+    setIsLoggedInStorage,
+    Axios,
+    AxiosForm,
+    setRoleStorage,
+    UploadScreenshots,
+    DeleteScreenshot,
+    setPushStorage,
 } from "../Actions"
 import { setAlert } from "../Alerts/actions"
 import ReactGA from "react-ga"
 
-export const thunkLoadUserData = (): ThunkAction<
-  void,
-  ReduxCombinedState,
-  null,
-  Action
+export const thunkAddPush = (push: Push): ThunkAction<
+    void,
+    ReduxCombinedState,
+    null,
+    Action
 > => async (dispatch) => {
-  dispatch(setLoading(true))
-  const data = await getUserData()
-  dispatch(setData(data))
-  dispatch(setLoading(false))
+    dispatch(setData({ push: push }))
+    await setPushStorage(push)
+}
+
+export const thunkLoadUserData = (): ThunkAction<
+    void,
+    ReduxCombinedState,
+    null,
+    Action
+> => async (dispatch) => {
+    dispatch(setLoading(true))
+    const data = await getUserData()
+    dispatch(setData(data))
+    dispatch(setLoading(false))
 }
 
 export const thunkSignUp = (
-  username: string,
-  password: string,
-  email: string
+    username: string,
+    password: string,
+    email: string
 ): ThunkAction<void, ReduxCombinedState, null, Action> => async (dispatch) => {
-  dispatch(setLoading(true))
-  try {
-    const url = `public/signup`
-    const response = await (await Axios()).post(url, {
-      username,
-      password,
-      email,
-    })
-    const {
-      data: { token },
-    } = response
+    dispatch(setLoading(true))
+    try {
+        const url = `public/signup`
+        const response = await (await Axios()).post(url, {
+            username,
+            password,
+            email,
+        })
+        const {
+            data: { token },
+        } = response
 
-    // TODO: do this in the api.
-    var role = UserRole.Dev
-    if (username === "mattruddy") {
-      role = UserRole.Admin
+        // TODO: do this in the api.
+        var role = UserRole.Dev
+        if (username === "mattruddy") {
+            role = UserRole.Admin
+        }
+        dispatch(setData({ token, username, isLoggedIn: true, role }))
+        await setRoleStorage(UserRole.Admin.toString())
+        await setTokenStorage(token)
+        await setUsernameStorage(username)
+        await setEmailStorage(email)
+        await setIsLoggedInStorage("true")
+        ReactGA.event({
+            category: "sign up",
+            action: "User signed up!",
+        })
+        dispatch(
+            setAlert({
+                message: "Signed Up!",
+                timeout: 3000,
+                show: true,
+                status: "success",
+            })
+        )
+    } catch (e) {
+        dispatch(
+            setAlert({
+                message: e.response.message,
+                apiResponseStatus: e.response.status,
+                timeout: 3000,
+                show: true,
+                status: "fail",
+            })
+        )
+        return console.log(e)
+    } finally {
+        dispatch(setLoading(false))
     }
-    dispatch(setData({ token, username, isLoggedIn: true, role }))
-    await setRoleStorage(UserRole.Admin.toString())
-    await setTokenStorage(token)
-    await setUsernameStorage(username)
-    await setEmailStorage(email)
-    await setIsLoggedInStorage("true")
-    ReactGA.event({
-      category: "sign up",
-      action: "User signed up!",
-    })
-    dispatch(
-      setAlert({
-        message: "Signed Up!",
-        timeout: 3000,
-        show: true,
-        status: "success",
-      })
-    )
-  } catch (e) {
-    dispatch(
-      setAlert({
-        message: e.response.message,
-        apiResponseStatus: e.response.status,
-        timeout: 3000,
-        show: true,
-        status: "fail",
-      })
-    )
-    return console.log(e)
-  } finally {
-    dispatch(setLoading(false))
-  }
 }
 
 export const thunkLogin = (
-  username: string,
-  password: string
+    username: string,
+    password: string
 ): ThunkAction<void, ReduxCombinedState, null, Action> => async (dispatch) => {
-  dispatch(setLoading(true))
-  try {
-    const url = `public/login`
-    const response = await (await Axios()).post(url, { username, password })
-    const {
-      data: { token },
-    } = response
+    dispatch(setLoading(true))
+    try {
+        const url = `public/login`
+        const response = await (await Axios()).post(url, { username, password })
+        const {
+            data: { token },
+        } = response
 
-    // TODO: do this in the api.
-    var role = UserRole.Dev
-    if (username === "mattruddy") {
-      role = UserRole.Admin
+        // TODO: do this in the api.
+        var role = UserRole.Dev
+        if (username === "mattruddy") {
+            role = UserRole.Admin
+        }
+        dispatch(setData({ token, username, isLoggedIn: true, role }))
+        await setRoleStorage(UserRole.Admin.toString())
+        await setTokenStorage(token)
+        await setUsernameStorage(username)
+        await setIsLoggedInStorage("true")
+        dispatch(
+            setAlert({
+                message: "Success",
+                timeout: 3000,
+                show: true,
+                status: "success",
+            })
+        )
+    } catch (e) {
+        dispatch(
+            setAlert({
+                message: e.response.message,
+                apiResponseStatus: e.response.status,
+                timeout: 3000,
+                show: true,
+                status: "fail",
+            })
+        )
+        return console.log(e)
+    } finally {
+        dispatch(setLoading(false))
     }
-    dispatch(setData({ token, username, isLoggedIn: true, role }))
-    await setRoleStorage(UserRole.Admin.toString())
-    await setTokenStorage(token)
-    await setUsernameStorage(username)
-    await setIsLoggedInStorage("true")
-    dispatch(
-      setAlert({
-        message: "Success",
-        timeout: 3000,
-        show: true,
-        status: "success",
-      })
-    )
-  } catch (e) {
-    dispatch(
-      setAlert({
-        message: e.response.message,
-        apiResponseStatus: e.response.status,
-        timeout: 3000,
-        show: true,
-        status: "fail",
-      })
-    )
-    return console.log(e)
-  } finally {
-    dispatch(setLoading(false))
-  }
 }
 
 export const thunkLoadProfile = (): ThunkAction<
-  void,
-  ReduxCombinedState,
-  null,
-  Action
+    void,
+    ReduxCombinedState,
+    null,
+    Action
 > => async (dispatch) => {
-  dispatch(setLoading(true))
-  try {
-    const url = `secure/profile`
-    const resp = await (await Axios()).get(url)
-    const {
-      data: { username, pageResponses, email },
-    } = resp
-    dispatch(
-      setData({
-        email,
-        username,
-        pwas: pageResponses as PWA[],
-      })
-    )
-    await setEmailStorage(email)
-    await setUsernameStorage(username)
-  } catch (e) {
-    dispatch(
-      setAlert({
-        message: e.response.message,
-        apiResponseStatus: e.response.status,
-        timeout: 3000,
-        show: true,
-      })
-    )
-    return console.error(e)
-  } finally {
-    dispatch(setLoading(false))
-  }
+    dispatch(setLoading(true))
+    try {
+        const url = `secure/profile`
+        const resp = await (await Axios()).get(url)
+        const {
+            data: { username, pageResponses, email },
+        } = resp
+        dispatch(
+            setData({
+                email,
+                username,
+                pwas: pageResponses as PWA[],
+            })
+        )
+        await setEmailStorage(email)
+        await setUsernameStorage(username)
+    } catch (e) {
+        dispatch(
+            setAlert({
+                message: e.response.message,
+                apiResponseStatus: e.response.status,
+                timeout: 3000,
+                show: true,
+            })
+        )
+        return console.error(e)
+    } finally {
+        dispatch(setLoading(false))
+    }
 }
 
 export const thunkSetUser = (
-  username: string,
-  email: string = "",
-  token: string
+    username: string,
+    email: string = "",
+    token: string
 ): ThunkAction<void, ReduxCombinedState, null, Action> => async (dispatch) => {
-  dispatch(setLoading(true))
-  dispatch(
-    setData({
-      token,
-      email,
-      username,
-      isLoggedIn: true,
-    })
-  )
-  await setEmailStorage(email)
-  await setTokenStorage(token)
-  await setUsernameStorage(username)
-  await setIsLoggedInStorage("true")
-  dispatch(setLoading(false))
+    dispatch(setLoading(true))
+    dispatch(
+        setData({
+            token,
+            email,
+            username,
+            isLoggedIn: true,
+        })
+    )
+    await setEmailStorage(email)
+    await setTokenStorage(token)
+    await setUsernameStorage(username)
+    await setIsLoggedInStorage("true")
+    dispatch(setLoading(false))
 }
 
 export const thunkLogout = (): ThunkAction<
-  void,
-  ReduxCombinedState,
-  null,
-  Action
+    void,
+    ReduxCombinedState,
+    null,
+    Action
 > => async (dispatch) => {
-  dispatch(setLoading(true))
-  dispatch(
-    setData({
-      token: "",
-      email: "",
-      username: "",
-      isLoggedIn: false,
-      pwas: [],
-    })
-  )
-  await setEmailStorage("")
-  await setTokenStorage("")
-  await setUsernameStorage("")
-  await setRoleStorage("")
-  await setIsLoggedInStorage("false")
-  dispatch(setLoading(false))
+    dispatch(setLoading(true))
+    dispatch(
+        setData({
+            token: "",
+            email: "",
+            username: "",
+            isLoggedIn: false,
+            pwas: [],
+        })
+    )
+    await setEmailStorage("")
+    await setTokenStorage("")
+    await setUsernameStorage("")
+    await setRoleStorage("")
+    await setIsLoggedInStorage("false")
+    dispatch(setLoading(false))
 }
 
 export const thunkSetEmail = (
-  email: string
+    email: string
 ): ThunkAction<void, ReduxCombinedState, null, Action> => async (dispatch) => {
-  dispatch(setData({ email }))
-  await setEmailStorage(email)
+    dispatch(setData({ email }))
+    await setEmailStorage(email)
 }
 
 export const setLoading = (isLoading: boolean) =>
-  ({
-    type: USER_SET_LOADING,
-    payload: isLoading,
-  } as const)
+    ({
+        type: USER_SET_LOADING,
+        payload: isLoading,
+    } as const)
 
 export const setData = (data: Partial<UserState>) =>
-  ({
-    type: USER_SET_DATA,
-    payload: data,
-  } as const)
+    ({
+        type: USER_SET_DATA,
+        payload: data,
+    } as const)
 
 export const setPWAS = (pwas: PWA[] | undefined) =>
-  ({
-    type: USER_SET_PWAS,
-    payload: pwas,
-  } as const)
+    ({
+        type: USER_SET_PWAS,
+        payload: pwas,
+    } as const)
 
 export const replaceApp = (app: PWA) =>
-  ({
-    type: USER_REPLACE_APP,
-    payload: app,
-  } as const)
+    ({
+        type: USER_REPLACE_APP,
+        payload: app,
+    } as const)
 
 export const addApp = (app: PWA) =>
-  ({
-    type: USER_ADD_APP,
-    payload: app,
-  } as const)
+    ({
+        type: USER_ADD_APP,
+        payload: app,
+    } as const)
 
 export const removeApp = (appId: number) =>
-  ({
-    type: USER_REMOVE_APP,
-    payload: appId,
-  } as const)
+    ({
+        type: USER_REMOVE_APP,
+        payload: appId,
+    } as const)
 
 export const thunkThirdPartyLogin = (
-  token: string
+    token: string
 ): ThunkAction<void, ReduxCombinedState, null, Action> => async (dispatch) => {
-  dispatch(setData({ token: token, isLoggedIn: true }))
+    dispatch(setData({ token: token, isLoggedIn: true }))
 }
 
 export const thunkAddPWA = (
-  name: string,
-  description: string,
-  url: string,
-  category: string,
-  icon: File,
-  screenshots: File[],
-  tags: string[]
+    name: string,
+    description: string,
+    url: string,
+    category: string,
+    icon: File,
+    screenshots: File[],
+    tags: string[]
 ): ThunkAction<void, ReduxCombinedState, null, Action> => async (dispatch) => {
-  dispatch(setLoading(true))
-  try {
-    const info = {
-      name: name,
-      description: description,
-      link: url,
-      category: category,
-      tags: tags,
+    dispatch(setLoading(true))
+    try {
+        const info = {
+            name: name,
+            description: description,
+            link: url,
+            category: category,
+            tags: tags,
+        }
+
+        const fd = new FormData()
+        fd.append("icon", icon)
+        screenshots.forEach((screenshot) => fd.append("screenshots", screenshot))
+        fd.append("info", JSON.stringify(info))
+
+        const requestUrl = `secure/pwas`
+        const response = await (await AxiosForm(fd)).post(requestUrl, fd)
+        const { data } = response
+        dispatch(addApp(data as PWA))
+        dispatch(
+            setAlert({
+                message: `${name} was submitted`,
+                timeout: 3000,
+                show: true,
+                status: "success",
+            })
+        )
+        //return data
+    } catch (e) {
+        dispatch(
+            setAlert({
+                message: e.response.message,
+                apiResponseStatus: e.response.status,
+                timeout: 3000,
+                show: true,
+                status: "fail",
+            })
+        )
+    } finally {
+        dispatch(setLoading(false))
     }
-
-    const fd = new FormData()
-    fd.append("icon", icon)
-    screenshots.forEach((screenshot) => fd.append("screenshots", screenshot))
-    fd.append("info", JSON.stringify(info))
-
-    const requestUrl = `secure/pwas`
-    const response = await (await AxiosForm(fd)).post(requestUrl, fd)
-    const { data } = response
-    dispatch(addApp(data as PWA))
-    dispatch(
-      setAlert({
-        message: `${name} was submitted`,
-        timeout: 3000,
-        show: true,
-        status: "success",
-      })
-    )
-    //return data
-  } catch (e) {
-    dispatch(
-      setAlert({
-        message: e.response.message,
-        apiResponseStatus: e.response.status,
-        timeout: 3000,
-        show: true,
-        status: "fail",
-      })
-    )
-  } finally {
-    dispatch(setLoading(false))
-  }
 }
 
 export const thunkDeletePWA = (
-  pwa: PWA
+    pwa: PWA
 ): ThunkAction<void, ReduxCombinedState, null, Action> => async (dispatch) => {
-  dispatch(setLoading(true))
-  try {
-    const requestUrl = `secure/pwas/${pwa.appId}`
-    const response = await (await Axios()).delete(requestUrl)
-    dispatch(removeApp(pwa.appId))
-    dispatch(
-      setAlert({
-        message: `${pwa.name} was removed`,
-        apiResponseStatus: response.status,
-        timeout: 3000,
-        show: true,
-        status: "success",
-      })
-    )
-  } catch (e) {
-    dispatch(
-      setAlert({
-        message: e.response.message,
-        apiResponseStatus: e.response.status,
-        timeout: 3000,
-        show: true,
-        status: "fail",
-      })
-    )
-    return console.log(e)
-  } finally {
-    dispatch(setLoading(false))
-  }
+    dispatch(setLoading(true))
+    try {
+        const requestUrl = `secure/pwas/${pwa.appId}`
+        const response = await (await Axios()).delete(requestUrl)
+        dispatch(removeApp(pwa.appId))
+        dispatch(
+            setAlert({
+                message: `${pwa.name} was removed`,
+                apiResponseStatus: response.status,
+                timeout: 3000,
+                show: true,
+                status: "success",
+            })
+        )
+    } catch (e) {
+        dispatch(
+            setAlert({
+                message: e.response.message,
+                apiResponseStatus: e.response.status,
+                timeout: 3000,
+                show: true,
+                status: "fail",
+            })
+        )
+        return console.log(e)
+    } finally {
+        dispatch(setLoading(false))
+    }
 }
 
 export const thunkUpdateApp = (
-  name: string,
-  description: string,
-  category: string,
-  appId: number,
-  tags: string[],
-  newScreenshots: File[],
-  deletedScreenshotIds: number[]
+    name: string,
+    description: string,
+    category: string,
+    appId: number,
+    tags: string[],
+    newScreenshots: File[],
+    deletedScreenshotIds: number[]
 ): ThunkAction<void, ReduxCombinedState, null, Action> => async (dispatch) => {
-  dispatch(setLoading(true))
-  try {
-    if (newScreenshots.length > 0)
-      await UploadScreenshots(newScreenshots, appId)
-    await Promise.all(
-      deletedScreenshotIds.map(async (x) => await DeleteScreenshot(x))
-    )
-    const requestUrl = `secure/pwas/${appId}`
-    const response = await (await Axios()).put(requestUrl, {
-      name,
-      description: description,
-      category: category,
-      tags: tags,
-    })
-    const { data } = response
-    dispatch(replaceApp(data as PWA))
-    dispatch(
-      setAlert({
-        message: `${name} was updated`,
-        apiResponseStatus: response.status,
-        timeout: 3000,
-        show: true,
-        status: "success",
-      })
-    )
-    return data
-  } catch (e) {
-    console.log(e)
-    dispatch(
-      setAlert({
-        message: e,
-        apiResponseStatus: e.response.status,
-        timeout: 3000,
-        show: true,
-        status: "fail",
-      })
-    )
-    return console.log(e)
-  } finally {
-    dispatch(setLoading(false))
-  }
+    dispatch(setLoading(true))
+    try {
+        if (newScreenshots.length > 0)
+            await UploadScreenshots(newScreenshots, appId)
+        await Promise.all(
+            deletedScreenshotIds.map(async (x) => await DeleteScreenshot(x))
+        )
+        const requestUrl = `secure/pwas/${appId}`
+        const response = await (await Axios()).put(requestUrl, {
+            name,
+            description: description,
+            category: category,
+            tags: tags,
+        })
+        const { data } = response
+        dispatch(replaceApp(data as PWA))
+        dispatch(
+            setAlert({
+                message: `${name} was updated`,
+                apiResponseStatus: response.status,
+                timeout: 3000,
+                show: true,
+                status: "success",
+            })
+        )
+        return data
+    } catch (e) {
+        console.log(e)
+        dispatch(
+            setAlert({
+                message: e,
+                apiResponseStatus: e.response.status,
+                timeout: 3000,
+                show: true,
+                status: "fail",
+            })
+        )
+        return console.log(e)
+    } finally {
+        dispatch(setLoading(false))
+    }
 }
 
 export const thunkSetHasReadInstall = (
-  hasReadInstall: boolean
+    hasReadInstall: boolean
 ): ThunkAction<void, ReduxCombinedState, null, Action> => async (dispatch) => {
-  await setHasReadInstallStorage(hasReadInstall ? "true" : "false")
-  dispatch(setData({ hasRead: hasReadInstall }))
+    await setHasReadInstallStorage(hasReadInstall ? "true" : "false")
+    dispatch(setData({ hasRead: hasReadInstall }))
 }
