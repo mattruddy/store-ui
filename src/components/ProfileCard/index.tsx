983770137f6c950256dc8c2ed62030d1b9e7a496
Matt <mjruddy94@gmail.com>
2020-06-18 10:11:04 -0400

    fix margin issue on mobile

--- src/components/ProfileCard/index.tsx
+++ src/components/ProfileCard/index.tsx
@@ -8,10 +8,11 @@ import {
 } from "ionicons/icons"
 import LinkItem from "./LinkItem"
 import "./styles.css"
-import { IonRow, IonCol, IonButton, IonIcon } from "@ionic/react"
+import { IonRow, IonCol, IonButton, IonIcon, IonGrid } from "@ionic/react"
 import { PWA } from "../../util/types"
 import AppImgs from "./AppImgs"
 import DataBox from "./DataBox"
+import { capitalize } from "../../util"
 
 export interface TotalAppData {
   totalInstalls: number
@@ -50,87 +51,91 @@ const ProfileCard: React.FC<ContainerProps> = ({
   pwas,
 }) => {
   return (
-    <IonRow className="bottom-line-border">
-      <IonCol className="ProfileCardLeftCol" size="12">
-        <div className="ProfileCardLeft">
-          {!isLoading && (
-            <img
-              alt="avatar"
-              className="ProfileCardImg icon line-around"
-              src={avatar ? avatar : "assets/icon/logo.png"}
-            />
-          )}
-          <div className="ProfileCardLeftInfo">
-            {username && (
-              <div>
-                <h1 style={{ paddingLeft: "8px", margin: "0" }}>
-                  {fullName ? fullName : `@${username}`}
-                </h1>
-                {fullName && (
-                  <h4
-                    className="text-color"
-                    style={{ margin: "0", padding: "8px" }}
-                  >{`@${username}`}</h4>
-                )}
-                <div className="ProfileLinksCont">
-                  {email && (
-                    <LinkItem url={`mailto:${email}`} logo={mailOutline} />
+    <IonGrid fixed>
+      <IonRow className="bottom-line-border">
+        <IonCol size="12">
+          <div className="ProfileCardLeft">
+            {!isLoading && (
+              <img
+                alt="avatar"
+                className="ProfileCardImg icon line-around"
+                src={avatar ? avatar : "assets/icon/logo.png"}
+              />
+            )}
+            <div className="ProfileCardLeftInfo">
+              {username && (
+                <div>
+                  <h1
+                    className={`${fullName ? "" : "text-color"}`}
+                    style={{ paddingLeft: "8px", margin: "0" }}
+                  >
+                    {fullName ? capitalize(fullName) : `@${username}`}
+                  </h1>
+                  {fullName && (
+                    <h4
+                      className="text-color"
+                      style={{ margin: "0", padding: "8px" }}
+                    >{`@${username}`}</h4>
                   )}
-                  {gitHub && (
-                    <LinkItem
-                      url={gitHub}
-                      logo={logoGithub}
-                      className="ion-color-github"
-                    />
-                  )}
-                  {linkedIn && (
-                    <LinkItem
-                      url={linkedIn}
-                      logo={logoLinkedin}
-                      color="linkedin"
-                    />
-                  )}
-                  {twitter && (
-                    <LinkItem
-                      url={twitter}
-                      logo={logoTwitter}
-                      color="twitter"
-                    />
-                  )}
+                  <div className="ProfileLinksCont">
+                    {email && (
+                      <LinkItem url={`mailto:${email}`} logo={mailOutline} />
+                    )}
+                    {gitHub && (
+                      <LinkItem
+                        url={gitHub}
+                        logo={logoGithub}
+                        className="ion-color-github"
+                      />
+                    )}
+                    {linkedIn && (
+                      <LinkItem
+                        url={linkedIn}
+                        logo={logoLinkedin}
+                        color="linkedin"
+                      />
+                    )}
+                    {twitter && (
+                      <LinkItem
+                        url={twitter}
+                        logo={logoTwitter}
+                        color="twitter"
+                      />
+                    )}
+                  </div>
                 </div>
+              )}
+            </div>
+          </div>
+          {location && (
+            <div className="text-color">
+              <IonIcon icon={locationOutline} />
+              {location}
+            </div>
+          )}
+          <div className="ProfileSubTitleBlock">
+            {header && (
+              <div
+                style={{
+                  padding: "16px",
+                }}
+              >
+                {header}
+              </div>
+            )}
+            {isMyProfile && (
+              <div>
+                <IonButton fill="outline" color="dark" routerLink="/settings">
+                  Edit
+                </IonButton>
               </div>
             )}
           </div>
-        </div>
-        {location && (
-          <div className="text-color" style={{ paddingLeft: "24px" }}>
-            <IonIcon icon={locationOutline} />
-            {location}
-          </div>
-        )}
-        <div className="ProfileSubTitleBlock">
-          {header && (
-            <div
-              style={{
-                padding: "32px",
-                paddingTop: "16px",
-              }}
-            >
-              {header}
-            </div>
-          )}
-          {isMyProfile && (
-            <div>
-              <IonButton fill="outline" color="dark" routerLink="/settings">
-                Edit
-              </IonButton>
-            </div>
-          )}
-        </div>
-      </IonCol>
-      {data && <DataBox data={data} />}
-      {pwas && <AppImgs pwas={pwas} />}
-    </IonRow>
+        </IonCol>
+        {data && <DataBox data={data} />}
+        {pwas && <AppImgs pwas={pwas} />}
+      </IonRow>
+    </IonGrid>
   )
 }
 
