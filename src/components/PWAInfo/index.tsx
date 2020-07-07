4053f36c6a3962d8280bc5085359b4878e14cf9d
Matt <mjruddy94@gmail.com>
2020-07-06 21:16:53 -0400

    fix view button overlap

--- src/components/PWAInfo/index.tsx
+++ src/components/PWAInfo/index.tsx
@@ -8,6 +8,8 @@ import {
   IonFabButton,
   IonCard,
   IonCardContent,
+  IonCol,
+  IonRow,
 } from "@ionic/react"
 import { ShareUrl, FormItem } from "../"
 import { PWA } from "../../util/types"
@@ -81,97 +83,110 @@ const PWAInfo: React.FC<ContainerProps> = ({
     <>
       <IonCard className="line-around">
         <IonCardContent>
-          <div className="PWAInfoHeader">
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <img alt="icon" className="PWAInfoIcon" src={pwa.icon} />
-              <div className="PWAInfoToolbar">
-                {isEdit ? (
-                  <FormItem
-                    name="Name"
-                    value={name!}
-                    onChange={(e) => setName!(e.detail!.value)}
-                    maxLength={25}
-                    showError={!noSpecialChars(name!)}
-                    errorMessage="No special chars allowed"
-                  />
-                ) : (
-                  <span style={{ padding: "8px", fontSize: "20px" }}>
-                    {pwa.name}
-                  </span>
-                )}
-                {isEdit ? (
-                  <FormItem name="Category" showError={false} errorMessage="">
-                    <CategoryOptions initValue={cat} onPress={setCat!} />
-                  </FormItem>
-                ) : (
-                  <small style={{ padding: "8px" }}>{pwa.category}</small>
-                )}
+          <IonRow>
+            <IonCol size="12" sizeSm="10">
+              <div className="PWAInfoHeader">
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <img alt="icon" className="PWAInfoIcon" src={pwa.icon} />
+                  <div className="PWAInfoToolbar">
+                    {isEdit ? (
+                      <FormItem
+                        name="Name"
+                        value={name!}
+                        onChange={(e) => setName!(e.detail!.value)}
+                        maxLength={25}
+                        showError={!noSpecialChars(name!)}
+                        errorMessage="No special chars allowed"
+                      />
+                    ) : (
+                      <span style={{ padding: "8px", fontSize: "20px" }}>
+                        {pwa.name}
+                      </span>
+                    )}
+                    {isEdit ? (
+                      <FormItem
+                        name="Category"
+                        showError={false}
+                        errorMessage=""
+                      >
+                        <CategoryOptions initValue={cat} onPress={setCat!} />
+                      </FormItem>
+                    ) : (
+                      <small style={{ padding: "8px" }}>{pwa.category}</small>
+                    )}
+                  </div>
+                </div>
               </div>
-            </div>
-            <IonButton
-              color="dark"
-              fill="outline"
-              href={pwa.link}
-              target="_blank"
-              onClick={onInstall}
-            >
-              VIEW <IonIcon style={{ marginLeft: "10px" }} icon={openOutline} />
-            </IonButton>
-          </div>
-          {pwa.username && (
-            <div style={{ marginLeft: "8px" }}>
-              <IonRouterLink routerLink={`/dev/${pwa.username}`}>
-                {`${pwa.fullName ? pwa.fullName : pwa.username}`}
-              </IonRouterLink>
-            </div>
-          )}
-          <div className="InfoStarBlock">
-            <IonFabButton className="InfoStarIcon" onClick={onClickStar}>
-              <IonIcon
-                icon={`${pwa.appRatings.hasRated ? starSharp : starOutline}`}
-              />
-            </IonFabButton>
-            <IonFabButton
-              className="InfoStarIcon primary-color"
-              onClick={openModal}
-            >
-              {pwa.ratingsCount}
-            </IonFabButton>
-          </div>
-          {isEdit ? (
-            <FormItem name="Tags" showError={false} errorMessage="">
-              <div style={{ padding: "10px", width: "100%" }}>
-                <ReactTagInput
-                  tags={tags!}
-                  onChange={(newTags) => {
-                    setTags!(newTags)
-                  }}
-                  validator={(tag) => {
-                    return tag.length <= 30
-                  }}
-                  removeOnBackspace={true}
-                  maxTags={5}
-                  placeholder="Enter to add"
+              {pwa.username && (
+                <div style={{ marginLeft: "8px" }}>
+                  <IonRouterLink routerLink={`/dev/${pwa.username}`}>
+                    {`${pwa.fullName ? pwa.fullName : pwa.username}`}
+                  </IonRouterLink>
+                </div>
+              )}
+              <div className="InfoStarBlock">
+                <IonFabButton className="InfoStarIcon" onClick={onClickStar}>
+                  <IonIcon
+                    icon={`${
+                      pwa.appRatings.hasRated ? starSharp : starOutline
+                    }`}
+                  />
+                </IonFabButton>
+                <IonFabButton
+                  className="InfoStarIcon primary-color"
+                  onClick={openModal}
+                >
+                  {pwa.ratingsCount}
+                </IonFabButton>
+              </div>
+              {isEdit ? (
+                <FormItem name="Tags" showError={false} errorMessage="">
+                  <div style={{ padding: "10px", width: "100%" }}>
+                    <ReactTagInput
+                      tags={tags!}
+                      onChange={(newTags) => {
+                        setTags!(newTags)
+                      }}
+                      validator={(tag) => {
+                        return tag.length <= 30
+                      }}
+                      removeOnBackspace={true}
+                      maxTags={5}
+                      placeholder="Enter to add"
+                    />
+                  </div>
+                </FormItem>
+              ) : (
+                pwa.tags.length > 0 && (
+                  <div>
+                    {pwa.tags.map((x, i) => (
+                      <IonChip key={i}>
+                        <IonLabel>{x}</IonLabel>
+                      </IonChip>
+                    ))}
+                  </div>
+                )
+              )}
+              <div className="PWAShareContainer">
+                <ShareUrl
+                  title={pwa.name}
+                  url={window.location.href.replace("mypwa", "pwa")}
                 />
               </div>
-            </FormItem>
-          ) : (
-            pwa.tags.length > 0 && (
-              <div>
-                {pwa.tags.map((x, i) => (
-                  <IonChip key={i}>
-                    <IonLabel>{x}</IonLabel>
-                  </IonChip>
-                ))}
-              </div>
-            )
-          )}
-          <div className="PWAShareContainer">
-            <ShareUrl
-              title={pwa.name}
-              url={window.location.href.replace("mypwa", "pwa")}
-            />
-          </div>
+            </IonCol>
+            <IonCol size="12" sizeSm="2">
+              <IonButton
+                color="dark"
+                fill="outline"
+                href={pwa.link}
+                target="_blank"
+                onClick={onInstall}
+              >
+                VIEW{" "}
+                <IonIcon style={{ marginLeft: "10px" }} icon={openOutline} />
+              </IonButton>
+            </IonCol>
+          </IonRow>
         </IonCardContent>
       </IonCard>
       <IonCard className="line-around">
